2f503d1ceea42d640a309db9260825643b066a59
Joey Perea <j.perea08@gmail.com>
2018-05-18 17:54:11 -0400

    changed home html and the specialty js

--- app/public/js/specialty.js
+++ app/public/js/specialty.js
@@ -1,5 +1,13 @@
 //lets get an array of all specialties
 $.get("/api/speciality", function(data){
   console.log(data);
+  //the data is an array of objects,
+  //var for the select tag
+  var dropdown = $("#")
+  for(var i = 0; i < data.length; i++){
+  	//add elements to the drop down
+
+  }
+
 });
     
