3ef252f75588d1f8359222d9a38584e8afc7cfa8
Joey Perea <j.perea08@gmail.com>
2018-05-18 23:44:33 -0400

    second view working

--- app/public/js/doctor-logic.js
+++ app/public/js/doctor-logic.js
@@ -6,11 +6,34 @@ $(document).ready(function(){
 	  console.log(data[0].choice);
 	  //take the choice and get doctors array
 	  $.get("/api/doctors/" + data[0].choice, function(docData){
-	  	console.log(docData);
+	  	console.log(docData.length);
 	  	//we have the array, create the elements
+	  	var container = $("#doctorShow");
+	  	for(var i = 0; i < docData.length; i++){
+	  		//create column
+	  		var column = $("<div>").attr("class", "column-2 w-col w-col-2");
+	  		var name = $("<div>Name: </div>").attr("class", "text-block-5").text(docData[i].name);
+	  		var specialty = $("<div>Specialty: </div>").text(docData[i].specialty);
+	  		var address = $("<div>Address: </div>").text(docData[i].address);
+	  		var city = $("<div>City :</div>").text(docData[i].city);
+	  		var state = $("<div>State: </div>").text(docData[i].state);
+	  		var zip = $("<div>Zip Code: </div>").text(docData[i].zipCode);
+	  		var phone = $("<div>Number: </div>").text(docData[i].phoneNumber);
+	  		var insurance = $("<div>Insurance: </div>").text(docData[i].insurance);
+	  		var button = $("<button>Select</button>").attr("data-id",docData[i].docID);
+	  		column.append(name, specialty,address,city,state,zip,phone,insurance,button);
+	  		container.append(column);
+	  	}
+
+	  	//listener to element
+		$("button").click(function(){
+			console.log("clicked");
+			console.log($(this).attr("data-id"));
+      	});
 	  });
 	});
 
+	
 });
 
 
